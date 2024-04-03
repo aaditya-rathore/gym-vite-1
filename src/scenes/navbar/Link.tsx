d4f9c84fd686362e15854d247a6772d3dd970a80
aaditya-rathore <aadityarathore29@gmail.com>
2024-04-04 01:01:56 +0530

    first push

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,24 @@
+import { SelectedPage } from '@/shared/types';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+type Props = {
+    page: string;
+    selectedPage: SelectedPage;
+    setSelectedPage: (value: SelectedPage) => void;
+}
+
+const Link = ({page,selectedPage,setSelectedPage,
+}: Props) => {
+  const lowercasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
+  return (
+    <AnchorLink
+      href={`#${lowercasePage}`}
+      className={`${selectedPage === lowercasePage ? "text-primary-500" : " "} hover:text-primary-300 transition duration-500`}
+      onClick={() => setSelectedPage(lowercasePage)}
+      >
+        {page}
+    </AnchorLink>
+  )
+}
+
+export default Link
